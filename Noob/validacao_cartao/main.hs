e8fa3554547d978f9e85ee5dcf7a68c20fe06bd3
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-03-28 20:06:37 -0300

    Still nothing about conversion. Actually, found out that you CAN'T convert IO types in actual types. But you can make them work out by using an IO function. Still don't know how tho.

--- Noob/validacao_cartao/main.hs
+++ Noob/validacao_cartao/main.hs
@@ -1,20 +1,25 @@
-readAndCheckCard = do
-                      putStrLn("Digite o numero do seu cartao de credito")
-                      x <- getLine
+main :: IO ()
+main = do
+          putStrLn("Digite o numero do seu cartao de credito")
+          x <- getLine
 
-                      -- Transforms the given input in an Integer
-                      read x::Int
+          -- Transforms the given input in an Integer
+          -- read x::Int -- read function only works for string values
+          y <- turnInt x
 
-                      -- Transforms the transformed int in a list of Ints.
-                      separateDigits x
+          -- Transforms the transformed int in a list of Ints.
+          separateDigits x
 
-                      -- Credit cards have 16 digits, if the read number doesn't, it's not valid.
-                      if (myLength x) == 16
-                        then putStrLn("Seu cartao eh valido")
-                        else putStrLn("Manda um cartao de verdade Zé ruela")
+          -- Credit cards have 16 digits, if the read number doesn't, it's not valid.
+          if (myLength x) == 16
+            then putStrLn("Seu cartao eh valido")
+            else putStrLn("Manda um cartao de verdade Zé ruela")
 
 
-separateDigits :: Integer -> [Int]
+turnInt :: String -> Int
+turnInt x = do return (read x)
+
+separateDigits :: Int -> [Int]
 separateDigits = map (read . return) . show
 
 myLength :: [Int] -> Int
